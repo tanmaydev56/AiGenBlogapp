1af864206db1d50dfe0f864a1b16852a21cf755d
Tanmay <tanmaysharma763@gmail.com>
2024-12-30 00:18:00 +0530

    vercel error publishable key

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,6 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider 
+    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
 
     >
     <html lang="en">
